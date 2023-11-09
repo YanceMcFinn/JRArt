64fa7f3218bffb0e140e1403feb93281a57f38e8
Sean Cole <seancole@Seans-MacBook-Pro.local>
2023-11-08 19:22:22 -0500

    Updated site name and favicon and fixed signature for Safari browser

--- .history/app/about/page_20231108191126.jsx
+++ .history/app/about/page_20231108191126.jsx
@@ -17,9 +17,9 @@ export default function About() {
             alt="me" />
             <div className='mt-3 w-9/12 mx-auto'>
                 <div className='title text-3xl justify-items-center'>About the Artist</div>
-                <div className='mb-2'>I can’t remember a time when I didn’t want to be an artist. I love playing with color and solving visual puzzles that I have created for myself. I’m always trying to stay out of my own way and allow things to happen spontaneously on the canvas, almost like a form of automatic writing. When everything just flows, it feels wonderful, and the best part is being able to share it with other people.</div>
+                <div className='mb-3'>I can’t remember a time when I didn’t want to be an artist. I love playing with color and solving visual puzzles that I have created for myself. I’m always trying to stay out of my own way and allow things to happen spontaneously on the canvas, almost like a form of automatic writing. When everything just flows, it feels wonderful, and the best part is being able to share it with other people.</div>
                 <div className='mx-auto sm:w-full sm-:pl-0 md:pl-20 md:w-6/12 flex justify-center'>
-                    <Signature className="fill-main-color" width="100%" height="100%"/>
+                    <Signature className="fill-main-color" width="750" height="200"/>
                 </div>
             </div>
         </div>
